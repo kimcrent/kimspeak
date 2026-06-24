3f0341a1cf51bd3cf6813fdb0c18da7e19f01ec9
k1epa <kimcrent@gmail.com>
2026-06-25 01:49:17 +0600

    Запуск нового ,mux  guilds

--- internal/httpserver/router.go
+++ internal/httpserver/router.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/kimcrent/kimspeak/internal/auth"
+	"github.com/kimcrent/kimspeak/internal/guilds"
 	"github.com/kimcrent/kimspeak/internal/health"
 	"github.com/kimcrent/kimspeak/internal/users"
 )
@@ -16,11 +17,14 @@ func (s *Server) NewRouter() http.Handler {
 
 	usersRepository := users.NewRepository(s.db)
 	authHandler := auth.NewHandler(usersRepository, s.cfg.JWTSecret)
+	guildsRepository := guilds.NewRepository(s.db)
+	guildsHandler := guilds.NewHandler(guildsRepository)
 
 	mux.HandleFunc("GET /health", healthHandler.Check)
 	mux.HandleFunc("/auth/register", authHandler.Register)
 	mux.HandleFunc("/auth/login", authHandler.Login)
 	mux.Handle("/me", authHandler.AuthMiddleware(http.HandlerFunc(authHandler.Me)))
+	mux.Handle("/guilds", authHandler.AuthMiddleware(http.HandlerFunc(guildsHandler.Create)))
 
 	return mux
 }
